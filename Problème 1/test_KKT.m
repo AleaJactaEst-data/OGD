12e7d54174c96b8485c21a5d2effcf511f4dd1ab
AleaJactaEst-data <81962949+AleaJactaEst-data@users.noreply.github.com>
2021-04-05 23:15:20 +0200

    Update test_KKT.m

--- Problème 1/test_KKT.m
+++ Problème 1/test_KKT.m
@@ -1,8 +1,12 @@
 function [kkt] = test_KKT(u, mu, A,b,C,d, EPS)
 % test des conditions KKT
+
+% gestion de la fonction si EPS n'est pas donnee
 if nargin <7
     EPS=1e-2;
 end
+
+% donne le critere KKT comme vu dans les TP
 kkt=all(C*u-d<EPS) && all(mu>-EPS) && all(abs(mu.*(C*u-d))<EPS)&& all(abs(A*u-b+C'*mu)<EPS);
 end
 
