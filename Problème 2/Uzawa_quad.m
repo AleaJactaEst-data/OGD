ebd8b43996aef525e2648166acdb039aea7ff0bb
AleaJactaEst-data <81962949+AleaJactaEst-data@users.noreply.github.com>
2021-04-05 23:05:22 +0200

    Update Uzawa_quad.m

--- Problème 2/Uzawa_quad.m
+++ Problème 2/Uzawa_quad.m
@@ -1,16 +1,22 @@
 function [ u,J ,mu,it] = Uzawa_quad( A,b,C,d,rho,mu0, espsi, maxiter)
 %A SDP, mu0 positif
+% prise en charge si maxiter non precise
 if nargin<8
     maxiter=1000;
 end
 
+%initialisation
 mu=mu0;
 
 diff = [1,1];
+% resolution pour u pour mu_0
 u=A\(b-C'*mu);
 
+% choix de mu
 mu=max(0,mu+rho*(C*u-d));
 Jk=1/2*u'*A*u-b'*u;
+
+% commence boucle jusqu'a au ax maxiter iteration
 it=1;
 while it<maxiter && all(diff>espsi)
     uk = u;
@@ -24,7 +30,4 @@ while it<maxiter && all(diff>espsi)
     diff=[norm(u-uk,inf), abs(J-Jk)];
     it=it+1;
 end
-
-
-
 end
